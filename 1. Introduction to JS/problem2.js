a3b9f141eb5bdb46fce8fdda889a8c6ca5a71518
anup patel <anupchandwani1926@gmail.com>
2022-09-06 00:05:24 +0530

    first

---
+++ 1. Introduction to JS/problem2.js
@@ -0,0 +1,6 @@
+var Name = "Anup Patel";
+console.log(Name);
+var FatherName = "Ramakant rai";
+console.log(FatherName);
+var mother_name = "pushpa devi";
+console.log(mother_name);
---
+++ 1. Introduction to JS/problem4.js
@@ -0,0 +1,12 @@
+//Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects//
+var name = "Anup Patel";
+var School = "Jai Hind Public School";
+var grade = "A";
+// Subject//
+var hindi = 78;
+var english = 60;
+var Science = 60;
+console.log(name);
+console.log(School);
+console.log(grade);
+console.log("Hindi",hindi,"English",english," Science",Science);
